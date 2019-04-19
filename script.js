10f00a1a69c7abf177e0188b484d3ae0999fc52f
Dmytro Shaturnyi <shaturnyj120293@gmail.com>
2019-04-19 17:57:19 +0300

    task 3

--- script.js
+++ script.js
@@ -84,9 +84,18 @@ let changeClass = ( classname, styleString ) => (
 
 changeClass ( "second-level-menu", "background-color: red!important;" )
 в консоли страницы, где есть элементы с классом second-level-menu, у всех элементов этого класса цвет фона должен измениться на красный*/
-
-
-
-
-
-
+let changeClass = ( classname, styleString ) => ( Array.from ( document.styleSheets )
+    .filter ( sheet => !sheet.href )
+        .map (
+            sheet => Array.from ( sheet.cssRules )
+                    .filter ( rule => rule.selectorText === `.{classname}` )
+        )
+           .filter ( item => item.length > 0 )
+               .map ( item => item[0].cssText.split ("}")
+                           .join ( `${styleString}}` )
+               )
+).length > 0 ? console.log ( "found" ) : 
+    document.head.appendChild (
+        document.createElement ( "style" )
+    ).textContent = `.${classname} {${styleString}}`
+changeClass ( "second-level-menu", "background-color: red!important;" )
